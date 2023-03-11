f1ed2248a6751e751eb27c2e9e17b9067d87ae20
Suraj Rampure <surajr@me.com>
2023-03-10 19:18:04 -0800

    add posters and mentors

--- scripts/build_site.py
+++ scripts/build_site.py
@@ -15,11 +15,22 @@ with open(template_path, 'r') as f:
     
 df = pd.read_csv(sheet_path)
 df = df.iloc[1:].reset_index(drop=True)
+df['Section'] = df['Group #'].str.split('-').str[0]
+
+mentors = pd.read_csv('2022-23 Student-Facing Capstone Scheduling Sheet - Sheet3.csv', 
+                      header=None)
+mentors.columns = ['Section', 'Mentors']
+posters = pd.read_csv('Poster links - Sheet1 (1).csv')
+posters['Group #'] = posters['File'].str.extract(r'([AB]\d{2,3}-\d)')
+
+df = df.merge(mentors, on='Section').merge(posters, on='Group #')
 
 def format_project(row):
+    mentor_label = 'Mentors' if ('and' in row['Mentors'] or ',' in row['Mentors']) else 'Mentor'
     return f'''
 <b>{row["Project Title"]}</b>
-<p>Group {row["Group #"]}: {row["Names"]}</p>
+<p>Group {row["Group #"]}: {row["Names"]} ({mentor_label}: {row['Mentors']})<br>
+<a href="{row['URL']}">Poster</a></p>
     '''
 
 def process_broad_area(area, block):
